983447328747804bcb6e8c5a2ffd7df9d7e153e9
yujiando11 <penandrisu602@gmail.com>
2023-11-25 16:39:55 +0900

    Created

---
+++ mypkg/talker.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+def cb(request, response):
+    if request.name == "上田隆一":
+        response.age = 44
+    else:
+        response.age = 255
+    
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
